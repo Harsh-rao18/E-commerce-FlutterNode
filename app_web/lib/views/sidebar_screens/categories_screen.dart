92e5a7ae0c9872feee93132feadf1f1c9ca210e3
Harsh-rao18 <harshrao64644@gmail.com>
2025-06-07 20:11:40 +0530

    add admin panel UI

---
+++ app_web/lib/views/sidebar_screens/categories_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  static const String id = '\categories-screen';
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Vendors'),
+    );
+  }
+}
---
+++ app_web/lib/views/sidebar_screens/upload_banner_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UploadBannerScreen extends StatelessWidget {
+  static const String id = '\bannersupload-screen';
+  const UploadBannerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Vendors'),
+    );
+  }
+}
